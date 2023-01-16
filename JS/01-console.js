98ef981baed81b9cd9a7a75faf017b3cd4553aa7
EBliujus <ev.bliujus@gmail.com>
2023-01-16 12:43:11 +0200

    cosole

---
+++ JS/01-console.js
@@ -0,0 +1,15 @@
+console.clear();
+/* 
+multi
+line
+comment
+ */
+
+// single line comment
+
+console.log(5);
+console.log(3, 14);
+console.log(3.14);
+console.log(1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
+
+

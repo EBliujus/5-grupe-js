f7a1cb8ad82fb9177be978f622280b4f44b22b6b
EBliujus <ev.bliujus@gmail.com>
2023-01-23 19:35:29 +0200

    biggest

--- JS/15-BiggestNum.js
+++ JS/15-BiggestNum.js
@@ -1,2 +1,14 @@
 console.clear()
 
+function biggestNum(list) {
+    if (!Array.isArray)
+    return 'ERROR: duok skaiciu sarase';
+}
+
+
+
+console.log(biggestNum());
+console.log(biggestNum(undefined));
+console.log(biggestNum(true));
+console.log(biggestNum('asddf'));
+console.log(biggestNum(null));

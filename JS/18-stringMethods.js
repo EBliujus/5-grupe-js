55705919c3f484d8e7e1bd0352677b908f521c7b
EBliujus <ev.bliujus@gmail.com>
2023-01-28 16:10:27 +0200

    String Methods done

--- JS/18-stringMethods.js
+++ JS/18-stringMethods.js
@@ -12,6 +12,14 @@ console.log(abc.includes('aas'));
 
 
 console.log('asdf'.repeat(5));
+console.log('a'.repeat(5));
+console.log('abc'.repeat(5));
+
+let str = '';
+for (let i = 0; i < 5; i++) {
+    str +='abc';
+}
+console.log(str);
 
 console.log('Labas rytas'.replace('rytas','vakaras'));
 
@@ -23,3 +31,43 @@ console.log('-------------------');
 
 const abcArray = abc.split('');
 console.log(abcArray);
+
+const sakinys = 'Labas rytas sakau tau';
+const zodynas = sakinys.split(' ');
+console.log(zodynas);
+
+console.log('Labas'.toUpperCase());
+console.log('LaBAs'.toUpperCase());
+console.log('LaBas'.toLowerCase());
+
+// vardas --> Vardas 
+// vARDAS --> Vardas
+// Vardas --> Vardas
+
+function formatName(name) {
+
+    return name[0].toUpperCase() + name.slice(1).toLowerCase();
+    /* const pirma = name[0];
+    const kitos = name.slice(1);
+
+    console.log(pirma, kitos);
+
+    return pirma.toUpperCase() + kitos.toLowerCase(); */
+}
+
+console.log(formatName('vardas'), '-->', 'Vardas');
+console.log(formatName('Vardas'), '-->', 'Vardas');
+console.log(formatName('vARDAS'), '-->', 'Vardas');
+console.log(formatName('VARDAS'), '-->', 'Vardas');
+console.log('----------------------');
+
+const num = 5;
+console.log(num.toString());
+console.log('' + num);
+
+console.log((777).toString());
+console.log('' + 777);
+console.log('----------------');
+
+const gg = '                      Good Game              ';
+console.log(gg.trim().length);
